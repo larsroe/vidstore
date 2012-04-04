96e9426f13702bffae1731dcb1cabe38152ceb5e
Lars Roe <lars@larsroe.com>
2012-04-04 13:14:37 -0400

    Simplify error handling in views

--- video/views.py
+++ video/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from vidstore.video.models import Video
+
 
 def listall(request):
     allvids = Video.objects.all()
@@ -8,33 +10,23 @@ def listall(request):
 
 def detail(request, id):
     user = request.user
-    try:
-        video = Video.objects.get(id=id)
-        return render_to_response('viewone.html', {'video':video, 'user':user})
-    except Video.DoesNotExist:
-        errors = ["Could not retrieve video details for id %s" % id]
-        return render_to_response('viewone.html', {'errors':errors, 'user':user})
+    video = get_object_or_404(Video, id=id)
+    return render_to_response('viewone.html', {'video':video, 'user':user})
 
+@login_required
 def rent_video(request):
     user = request.user
-    try:
-        video = Video.objects.get(id=request.POST['id'])
-        video.rent_video(user.username)
-        messages = ["Successfully rented %s" % video.title]
-        return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
-    except Video.DoesNotExist:
-        errors = ["Could not rent video"]
-        return render_to_response('viewone.html', {'errors':errors, 'user':user})
+    video = get_object_or_404(Video, id=request.POST.get('id'))
+    video.rent_video(user.username)
+    messages = ["Successfully rented %s" % video.title]
+    return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
 
+@login_required
 def return_video(request):
     user = request.user
-    try:
-        video = Video.objects.get(id=request.POST['id'])
-        video.return_video(user)
-        messages = ["Successfully returned %s" % video.title]
-        return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
-    except Video.DoesNotExist:
-        errors = ["Unable to return video"]
-        return render_to_response('viewone.html', {'errors':errors, 'user':user})
+    video = get_object_or_404(Video, id=request.POST.get('id'))
+    video.return_video(user)
+    messages = ["Successfully returned %s" % video.title]
+    return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
 
 
