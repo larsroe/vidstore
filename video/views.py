c46de4ef1a7a231b0da6cae89dcb58615c385053
Lars Roe <lars@larsroe.com>
2012-04-04 11:28:21 -0400

    Do not use locals()

--- video/views.py
+++ video/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from vidstore.video.models import Video
-import pprint
 
 def listall(request):
     allvids = Video.objects.all()
@@ -11,21 +10,21 @@ def detail(request, id):
     user = request.user
     try:
         video = Video.objects.get(id=id)
-        return render_to_response('viewone.html', locals())
-    except KeyError:
-        errors = ["Could not retrieve video details"]
-        return render_to_response('viewone.html', locals())
+        return render_to_response('viewone.html', {'video':video, 'user':user})
+    except Video.DoesNotExist:
+        errors = ["Could not retrieve video details for id %s" % id]
+        return render_to_response('viewone.html', {'errors':errors, 'user':user})
 
 def rent_video(request):
     user = request.user
     try:
         video = Video.objects.get(id=request.POST['id'])
         video.rent_video(user.username)
-        messages = [ "Successfully rented %s" % video.title ]
-        return render_to_response('viewone.html', locals())
-    except KeyError:
-        errors = ["Could not rent %s" % video.title ]
-        return render_to_response('viewone.html', locals())
+        messages = ["Successfully rented %s" % video.title]
+        return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
+    except Video.DoesNotExist:
+        errors = ["Could not rent video"]
+        return render_to_response('viewone.html', {'errors':errors, 'user':user})
 
 def return_video(request):
     user = request.user
@@ -33,9 +32,9 @@ def return_video(request):
         video = Video.objects.get(id=request.POST['id'])
         video.return_video(user)
         messages = ["Successfully returned %s" % video.title]
-        return render_to_response('viewone.html', locals())
-    except KeyError:
-        errors = ["Unable to return %s" % video.title]
-        return render_to_response('viewone.html', locals())
+        return render_to_response('viewone.html', {'video':video, 'user':user, 'messages':messages})
+    except Video.DoesNotExist:
+        errors = ["Unable to return video"]
+        return render_to_response('viewone.html', {'errors':errors, 'user':user})
 
 
